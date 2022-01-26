b5ff6116a68f6fa7bf54a884ecc1f0fe0c7b32a9
nelsongalicia <nelson.galicia.carrero.al@iespoligonosur.org>
2022-01-26 13:59:51 +0100

    en esta versión se agregó mediante los comentarios la operación aritmética que hace el programa

--- src/ventaFuteriaAnual.java
+++ src/ventaFuteriaAnual.java
@@ -4,13 +4,16 @@ public class ventaFuteriaAnual {
 
 	public static void main(String[] args) {
 	
+	//se definen dos constantes con un tipo de dato primitivo con los valores establecidos para la operacion 
 	final double precioM= 2.35;
 	final double precioP= 1.95;
 	
+	//se define el tipo double porque se solicita las ventas en kilos
 	double manzanaPrimer,manzanaSegundo, peraPrimer, peraSegundo;
 	
 	Scanner  sc= new Scanner (System.in);
 	
+	//se comienzan a guardar los datos en las variables creadas
 	System.out.println("Venta manzana primer semestre");
 	manzanaPrimer= sc.nextDouble();
 	
@@ -23,14 +26,29 @@ public class ventaFuteriaAnual {
 	System.out.println("Venta pera segundo semestre");
 	peraSegundo=sc.nextDouble();
 	
+	/**
+	 * Operacion aritmetica 
+	 * Como el ejercicio nos está pidiendo calcular los beneficios anuales de las manzanas y de las peras
+	 * lo primero que se va a hacer es que se va a dividir el año por dos semestre.
+	 * 
+	 * Aclarado lo anterior, la operación aritmética se hace de la siguiente manera:
+	 * 	Manzana:
+	 * 1- precioM * manzanaPrimer + precioM * manzanaSegundo para obtener el resultado final (opMan)
+	 * 
+	 * 
+	 * 	Pera:
+	 * 2- precioP * peraPrimer + precioP * peraSegundo para obtner el resultado final (opPer)
+	 * 
+	 * 	TOTAL:
+	 * Se suma el resultado del precio final de las manzanas y peras totalAnual= opMan + opPer;
+	 * 
+	 */
+	
 	double opMan= precioM * manzanaPrimer + precioM * manzanaSegundo;
 		
 	double opPer= precioP * peraPrimer + precioP * peraSegundo; 
 	
 	double totalAnual= opMan + opPer;
-	
-	//double totalAnual= precioM * manzanaPrimer + precioM * manzanaSegundo;
-	//totalAnual += (precioP * peraPrimer + precioP * peraSegundo);
 	
 	System.out.println("Los beneficios anuales de las manzanas y peras son" + totalAnual);
 	
